76cbd3c194697479cbeec9851877bb7b321b847c
Martin Hodges <martin.hodges@requillion-solutions.com>
2026-01-30 21:28:56 +0000

    fix: SSE response type converter error

--- src/main/java/com/requillion/solutions/inventory/controller/SseController.java
+++ src/main/java/com/requillion/solutions/inventory/controller/SseController.java
@@ -1,6 +1,5 @@
 package com.requillion.solutions.inventory.controller;
 
-import com.requillion.solutions.inventory.exception.NotAuthorizedException;
 import com.requillion.solutions.inventory.model.User;
 import com.requillion.solutions.inventory.security.RequestContext;
 import com.requillion.solutions.inventory.security.UserContext;
@@ -9,7 +8,9 @@ import com.requillion.solutions.inventory.service.InventoryService;
 import com.requillion.solutions.inventory.util.LoggerUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -25,17 +26,20 @@ public class SseController {
     private final InventoryService inventoryService;
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable UUID inventoryId) {
+    public ResponseEntity<?> subscribe(@PathVariable UUID inventoryId) {
         LoggerUtil.debug(log, "SSE subscribe request for inventory %s", inventoryId);
         RequestContext context = UserContext.getContext();
         User user = context.getUser();
 
         if (!inventoryService.canUserViewInventory(user, inventoryId)) {
-            throw new NotAuthorizedException(
-                    "You do not have access to this inventory",
-                    "Inventory: %s, User: %s", inventoryId, user.getId());
+            LoggerUtil.error(log, "SSE subscribe denied: Inventory: %s, User: %s", inventoryId, user.getId());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("{\"error\":\"FORBIDDEN\",\"message\":\"You do not have access to this inventory\",\"status\":403}");
         }
 
-        return eventService.subscribe(inventoryId, user);
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(eventService.subscribe(inventoryId, user));
     }
 }
